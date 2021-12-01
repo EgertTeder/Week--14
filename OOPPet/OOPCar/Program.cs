8461f9bc3684004017ab3e03dddd9e240b09e75b
Opilane TTHK <opilane@tthk.ee>
2021-12-01 10:26:32 +0200

    OOPPet

---
+++ OOPPet/OOPCar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOPCar
+{
+    class Program
+    {
+        class Car
+        {
+            string name;
+            string modelname;
+            string registration = "123456";
+            string color;
+            int odometer;
+            int fueltank;
+
+            public Car(string _anme, string _modelname, string _registration, string _color)
+            {
+                mark = _mark;
+                model = _model;
+                regnumber = _regNumber;
+                color = _color;
+                odom = 0;
+                fueltank = 68;
+                Console.WriteLine($"{mark} {model} {registration} has been created");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("Honda", "Civic", "123456", "Blaxk");
+        }
+    }
+}
---
+++ OOPPet/OOPPet/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace OOPPet
+{
+    class Program
+    {
+        class Pet
+        {
+            string name;
+            int age;
+            double weight;
+
+            public Pet(string _name)
+            {
+                name = _name;
+                age = 0;
+                weight = 0.1;
+                Console.WriteLine($"Congrulations! You have adoped {name}!");
+            }
+            public double Weight
+            {
+                get { return weight; }
+            }
+            
+        }
+        public void ShowPetData()
+        {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Weight: {weight}");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("Nom-Nom");
+            weight += 0.2;
+        }
+        public void Run()
+        {
+            Console.WriteLine("spin-spin-spin");
+            weight -= 0.1;
+        }
+        public void getOlder()
+        {
+            age++;
+        }
+        static void Main(string[] args)
+        {
+            Pet myPet = new Pet("Hamster");
+            myPet.ShowPetData();
+
+            for (int i = 0; i < 10; i++)
+            {
+                myPet.Eat();
+                Console.WriteLine($"Current weight: {myPet.Weight}");
+                while(myPet.weight < 0.7)
+                {
+                    myPet.Run;
+                }
+                //if (myPet.Weight > 0.7)
+                //{
+                //    myPet.Run();
+                //}
+
+                myPet.GetOlder();
+            }
+            myPet.ShowPetData();
+
+
+        }
+    
+    }
+
+}
+
+
+
+
+
+
+
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
